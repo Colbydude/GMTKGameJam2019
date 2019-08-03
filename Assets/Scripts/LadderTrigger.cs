3ff32ce31f3c17507ed982fb13a57f790f6bcf61
Colby Terry <colby@voidteam.net>
2019-08-03 13:53:03 -0700

    Add basic ladder trigger.

---
+++ Assets/Scripts/LadderTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderTrigger : MonoBehaviour
+{
+    public enum TriggerType {
+        Ladder,
+        Bridge,
+        Surfboard,
+        Platform,
+        Seesaw,
+        Helicopter
+    };
+
+    public GameObject player;
+    public TriggerType triggerType = TriggerType.Ladder;
+
+    private SpriteRenderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _renderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) < 2) {
+            _renderer.enabled = true;
+        } else {
+            _renderer.enabled = false;
+        }
+    }
+}
