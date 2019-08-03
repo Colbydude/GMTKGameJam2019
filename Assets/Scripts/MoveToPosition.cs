f80868fd69fdffbfdb97bcbb58b999c66c4ee92f
Colby Terry <colby@voidteam.net>
2019-08-02 21:34:51 -0700

    Add basic MoveToPosition script, sans scene switch. Pog.

---
+++ Assets/Scripts/MoveToPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MoveToPosition : MonoBehaviour
+{
+    [SerializeField] Object sceneToGoTo;
+    [SerializeField] float xPosition;
+    [SerializeField] float yPosition;
+
+    BoxCollider2D _collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Cllided With: " + collider.gameObject.name);
+
+        Transform playerTransform = collider.gameObject.GetComponent<Transform>();
+
+        playerTransform.position = new Vector3(xPosition, yPosition, 0);
+    }
+}
