d544385b568af0487b5e5a417fba74a791322901
Jordan Hill <jordan.hill@comcast.net>
2019-08-02 20:46:36 -0700

    Player moves
    Pog

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float gravity;
+    public float walkSpeed;
+    public float jumpSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        //get axis, should work for keyboard or controller
+        float xDir = Input.GetAxis("Horizontal");
+        float yDir = gravity * Time.deltaTime;
+
+        //translate character based on speed
+        xDir *= (walkSpeed * Time.deltaTime);
+        transform.Translate(xDir, yDir, 0);
     }
 }
